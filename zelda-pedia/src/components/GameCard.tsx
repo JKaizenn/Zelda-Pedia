57a143745e42538e1288e5ca5bde521ae1b550ad
Jessen Forbush <jessenforbush@gmail.com>
2026-05-12 15:07:43 -0400

    updating card styling

--- zelda-pedia/src/components/GameCard.tsx
+++ zelda-pedia/src/components/GameCard.tsx
@@ -6,8 +6,8 @@ interface GameCardProps {
 
 function GameCard({ game }: GameCardProps) {
     return (
-        <div>
-            <h2>{game.name}</h2>
+        <div className="bg-zinc-900 text-yellow-400 p-6 rounded-lg mx-auto">
+            <h2 className="text-2x1 font-bold">{game.name}</h2>
             <p>{game.released_date}</p>
             <p>{game.developer}</p>
             <p>{game.description}</p>
